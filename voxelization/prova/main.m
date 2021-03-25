3fefbfe289351f2f432d9cc9c43b1d2d2738cfa7
Michelangelo Maddinelli <michelangelo.maddinelli@gmail.com>
2021-03-26 00:26:00 +0100

    add mesh_voxelisation and tried in "prova"

--- voxelization/prova/main.m
+++ voxelization/prova/main.m
@@ -2,6 +2,7 @@ close all
 clear all
 clc
 
-stldemo;
-volume=polygon2voxel(fv,[100 100 100],'auto');
+%stldemo;
+%volume=polygon2voxel(fv,[100 100 100],'auto');
+volume=VOXELISE(20,20,20,'femur.stl','xyz');
 [vol_handle1]=VoxelPlotter(volume,1);
