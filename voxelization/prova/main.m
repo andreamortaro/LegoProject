7d3ee1df4502a4c005aafb8be39a7460cd339608
Michelangelo Maddinelli <michelangelo.maddinelli@gmail.com>
2021-03-27 00:31:12 +0100

    ...

--- voxelization/prova/main.m
+++ voxelization/prova/main.m
@@ -4,5 +4,5 @@ clc
 
 %stldemo;
 %volume=polygon2voxel(fv,[100 100 100],'auto');
-volume=VOXELISE(20,20,20,'femur.stl','xyz');
+volume=VOXELISE(40,40,40,'femur.stl','xyz');
 [vol_handle1]=VoxelPlotter(volume,1);
